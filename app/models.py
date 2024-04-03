33a30148aca7b00ca867906ce8b120b4d3c6a672
Ezra Muir <ezramuir12@gmail.com>
2024-04-02 21:59:36 -0500

    Exercise 2 - Movies Database table (3 marks)

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
